091fd584051262b02426054b2e276afa8ec33c2b
andy.bao <chinazzlm@gmail.com>
2015-12-19 16:29:25 +0800

    update

---
+++ chinazzlm-base-test/src/test/java/com/chinazzlm/chinazzlm/algorithm/CommonSort.java
@@ -0,0 +1,17 @@
+package com.chinazzlm.chinazzlm.algorithm;
+/**
+ * 常用排序算法
+ * @author andy.bao
+ * @date 2015年12月19日
+ */
+public interface CommonSort {
+	/**
+	 * 小鸡问题
+	 * 公鸡母鸡小鸡的问题有一百块钱,公鸡5元一只,母鸡3元一只,小鸡1元一只,问分别能买多少只公鸡,母鸡,小鸡?
+	 * x,y,z  打印出就好
+	 * @param x
+	 * @param y
+	 * @param z
+	 */
+    public void chick(int x,int y,int z);
+}
---
+++ chinazzlm-base-test/src/test/java/com/chinazzlm/chinazzlm/design/CommonDesign.java
@@ -0,0 +1,9 @@
+package com.chinazzlm.chinazzlm.design;
+/**
+ * 常用设计模式
+ * @author andy.bao
+ * @date 2015年12月19日
+ */
+public interface CommonDesign {
+
+}
